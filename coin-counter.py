ab11c0f537e69b36e3600577defff160530e0d6f
Jonas <jonas-wirth@gmx.ch>
2024-12-16 15:45:44 +0100

    implement watershed to find coins

--- coin-counter.py
+++ coin-counter.py
@@ -156,15 +156,55 @@ def transform_homography(img, corners):
     dst = cv.warpPerspective(img, transform_mat, (IMG_WIDTH, IMG_HEIGHT), flags=cv.INTER_LINEAR)
     
     if DEBUG:
-        show_and_save_image_debug(5, dst)
+        show_and_save_image_debug(5, dst, cmap="viridis")
+    return dst
+
+def find_coins(img):
+    #Threshold the image
+    gray = np.array(cv.cvtColor(img,cv.COLOR_BGR2GRAY) * 255, dtype="uint8")
+    ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+    
+    if DEBUG:
+        show_and_save_image_debug(6, thresh)
+    
+    #Get the starting points for the watershed
+    kernel =  np.ones((3,3), dtype="uint8")
+    opening = cv.morphologyEx(thresh,cv.MORPH_OPEN,kernel, iterations = 2)
+    sure_bg = cv.dilate(thresh,kernel, iterations=3)
+    dist_transform = cv.distanceTransform(opening,cv.DIST_L2,5)
+    ret, sure_fg = cv.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+    sure_fg = np.array(sure_fg, dtype="uint8")
+    unknown = cv.subtract(sure_bg, sure_fg)
+
+    if DEBUG:
+        show_and_save_image_debug(7, sure_fg)
+    
+    ret, markers = cv.connectedComponents(sure_fg)
+    markers = markers + 1
+    markers[unknown == 255] = 0
+    if DEBUG:
+        show_and_save_image_debug(99, markers, cmap="prism")
+
+    markers = cv.watershed(img, markers)
+    img[markers == -1] = [255,0,0]
+
+    if DEBUG:
+        show_and_save_image_debug(8, img)
 
 
-img = plt.imread("static.png") ##Todo replace with live image
-img = np.array(cv.cvtColor(img, cv.COLOR_BGR2GRAY) * 255, dtype="uint8")
 
-corners = detect_corners(img)
+
+img = cv.imread("static.png") ##Todo replace with live image
+
+plt.imshow(img)
+plt.show()
+
+img_gray = np.array(cv.cvtColor(img, cv.COLOR_BGR2GRAY) * 255, dtype="uint8")
+
+corners = detect_corners(img_gray)
 
 img_transformed = transform_homography(img, corners)
+find_coins(img_transformed)
 
-plt.imshow(img, cmap="gray")
+plt.imshow(img)
 plt.show()
